2360c1010a5fcbf54b68eff48671ad77f4dd21f1
KonstantinShilkov <shilkv1991@gmail.com>
2023-06-15 22:38:10 +0300

    delete tasks for review

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const ul = document.querySelector('.todo-list');
 const input = document.querySelector('.new-todo');
 
+
+
 function createListItem(item) {
 
     const li  = document.createElement('li');
@@ -14,9 +16,10 @@ function createListItem(item) {
     input.setAttribute('class','toggle');
     input.setAttribute('type','checkbox'); 
     button.setAttribute('class','destroy');
+    // button.setAttribute('onclick','deleteTask()');
 
     label.textContent = item.text; 
-    li.setAttribute('id',item.id );
+    li.setAttribute('data-id',item.id );
     
     const cond = item.completed == true? "completed" : "" ; 
     li.setAttribute('class',cond); 
@@ -61,12 +64,40 @@ function createNewTask() {
     tasksList.push({id: randomId(1 , 10000), text: newTask, completed: false });
        localStorage.setItem('mylist', JSON.stringify(tasksList));
        cleanInput();
-       renderTasks()
+       renderTasks();
     };
 };
 
 renderTasks();
 
-
+const deleteButtons = document.querySelectorAll('.destroy'); // рассказать как я не мог получить колекцию node =)))
    
-    
+    deleteButtons.forEach(button => {
+        button.addEventListener('click', function deleteTask (){
+           
+             // const clickedButtonId = button.parentNode.parentNode.dataset.id
+            console.log(tasksList)
+        
+            const clickedButton = button.parentNode.parentNode
+            clickedButton.remove()
+            const clickedButtonId = button.parentNode.parentNode.dataset.id
+            const clickedButtonIndex = tasksList.findIndex(task => task.id == clickedButtonId);
+            console.log(clickedButtonIndex)
+            tasksList.splice(clickedButtonIndex,1);
+            localStorage.setItem('mylist', JSON.stringify(tasksList));
+        
+            // console.log(clickedButton)
+            // console.log(clickedButtonId)
+            // console.log(clickedButtonIndex)
+            // console.log(clickedLi)
+
+
+
+
+            //   const clickedLi = clickedButton.parentNode.parentNode
+            //   const clickedId = console.log(clickedLi)
+
+
+        });
+
+    })
